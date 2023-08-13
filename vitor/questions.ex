59d4b3fa5f849f3c3fce419060079b125dc81ad7
VitorLelis <vitorll9090@gmail.com>
2023-08-13 13:40:57 +0100

    6 to 12

--- vitor/questions.ex
+++ vitor/questions.ex
@@ -1,4 +1,4 @@
-defmodule Question do
+defmodule Questions do
   # 1
   @spec enumFromTo(integer(), integer()) :: list(integer())
   def enumFromTo(first, last) do
@@ -37,4 +37,67 @@ defmodule Question do
 
   defp do_reverse([], acc), do: acc
   defp do_reverse([head | tail], acc), do: do_reverse(tail, [head | acc])
+
+  # 6
+  @spec take(list(), integer()) :: list()
+  def take([],_), do: []
+  def take(_, 0), do: []
+  def take([head | tail], n), do: [head | take(tail, n-1)]
+
+  # 7
+  @spec drop(list(),integer()) :: list()
+  def drop([],_), do: []
+  def drop(list,0), do: list
+  def drop([_|tail], n), do: drop(tail,n-1)
+
+  # 8
+  @spec zip(list(), list()) :: list()
+  def zip([],_), do: []
+  def zip(_,[]), do: []
+  def zip([a | b], [c | d]), do: [{a,c} | zip(b,d)]
+
+  # 9
+  # Numbers between 7 and 14 not working
+  @spec replicate(integer(),integer()) :: list(integer())
+  def replicate(0,_), do: []
+  def replicate(n,x), do: [x | replicate(n-1,x)]
+
+  # 10
+  # Define Generic types ?
+  # Alias for list ?
+  @spec intersperse(list(integer()),integer()) :: list(integer())
+  def intersperse([], _), do: []
+  def intersperse([head | tail],x) do
+    l = length([head | tail])
+    cond do
+      l > 2 ->
+        [head | [x | intersperse(tail,x)]]
+      l == 2 ->
+        [head | [x | tail]]
+      true ->
+        [head | tail]
+    end
+  end
+
+  # 11
+  @spec group(list()) :: list(list())
+  def group([]), do: []
+  def group([head | tail]) do
+     [auxgroup([head | tail], head) | Enum.drop_while(tail, fn(t) -> t == head end) |> group()]
+  end
+
+  defp auxgroup([], _), do: []
+  defp auxgroup([head | tail], x) do
+    if head == x do
+      [head | auxgroup(tail, x)]
+    else
+      []
+    end
+  end
+
+  # 12
+  @spec otherconcat(list(list())) :: list()
+  # def otherconcat(list), do: List.flatten(list)
+  def otherconcat([]), do: []
+  def otherconcat([head | tail]), do: head ++ otherconcat(tail) # Is there any faster way?
 end
